fec0d0b3ade5990343ecefdc80283a9ff76138c0
perchall <>
2012-01-31 17:54:57 +0000

    move the globaltag into the steering file. it may differ for each dataset.

--- ACFrameworkModules/FinalTreeFiller/FinalTreeFiller_cfg.py
+++ ACFrameworkModules/FinalTreeFiller/FinalTreeFiller_cfg.py
@@ -17,14 +17,14 @@ process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
 
 ###############
 #steering parameters#
-inputPath, outputPath, jobName, isData, jsonFile = Steering.LoadUserParameters()
+inputPath, outputPath, jobName, globaltag, isData, jsonFile, pileUpDistributionMC = Steering.LoadUserParameters()
 numberOfEvents = -1
 ignoreFilter = True # but always ignore TauMotherProducer until its ready
 printEvents = 0	#print generator event
 triggerTag = "HLT"
 minTau = 1 #minimum of selected taus
-doPileUpReweighting = True
-pileUpDistributionMC = "MCPileUpDistMaker/PU_S3_and_PU_S4_InTime"
+doPileUpReweighting = False
+pileUpReweightingType = "observed" #(true or observed) choose according to https://twiki.cern.ch/twiki/bin/view/CMS/PileupMCReweightingUtilities
 ###############
 
 ### scan all files located in inputPath
@@ -38,8 +38,13 @@ process.maxEvents = cms.untracked.PSet(
     input = cms.untracked.int32(numberOfEvents)
 )
 
+dataPileUpFilename = ''
 if doPileUpReweighting:
-    dataPileUpFilename = PileUpReweighting.createDataPileUpFile(jsonFile)
+    dataPileUpFilenameTrue, dataPileUpFilenameObs = PileUpReweighting.createDataPileUpFile(jsonFile)
+    if pileUpReweightingType == "true":
+        dataPileUpFilename = dataPileUpFilenameTrue
+    elif pileUpReweightingType == "observed":
+        dataPileUpFilename = dataPileUpFilenameObs
 
 ### select runs from JSON file
 if isData:
@@ -48,11 +53,7 @@ if isData:
     lumiString = jsonList.getCMSSWString()#convert into compact format needed by CMSSW
     process.source.lumisToProcess = cms.untracked.VLuminosityBlockRange(lumiString.split(','))
 
-if isData:
-    process.GlobalTag.globaltag = 'GR_P_V27::All'
-else:
-    process.GlobalTag.globaltag = 'START44_V7::All'
-
+process.GlobalTag.globaltag = globaltag
 print 'Using the GlobalTag ',process.GlobalTag.globaltag
 
 motherPdgID = 0
@@ -80,13 +81,14 @@ process.load("CommonTools.PrimVtxSelector.PrimVtxSelector_cfi")
 process.load("RecoTauTag.KinematicTau.InputTrackSelector_cfi")
 process.InputTrackSelector.minTau = cms.untracked.uint32(minTau)
 process.InputTrackSelector.filterTaus = cms.untracked.bool(False)
-process.load("RecoTauTag.KinematicTau.ThreeProngInputSelector_cfi")
-process.ThreeProngInputSelector.minTau = cms.untracked.uint32(minTau)
+process.load("RecoTauTag.KinematicTau.ThreeProngInputSelector_cff")
+process.ThreeProngInputSelectorStep1.minTau = cms.untracked.uint32(minTau)
+process.ThreeProngInputSelectorStep2.minTau = cms.untracked.uint32(minTau)
 process.load("RecoTauTag.KinematicTau.kinematictauAdvanced_cfi")
 process.KinematicTauProducer.minKinTau = cms.untracked.uint32(minTau)
-process.KinematicTauProducer.primVtx = cms.InputTag("ThreeProngInputSelector","primVtx")
-process.KinematicTauProducer.selectedTauCandidates = cms.InputTag("ThreeProngInputSelector","InputTauRefs")
-process.KinematicTauProducer.inputTracks = cms.InputTag("ThreeProngInputSelector","InputTracks")
+process.KinematicTauProducer.primVtx = cms.InputTag("ThreeProngInputSelectorStep2", "primVtx")
+process.KinematicTauProducer.selectedTauCandidates = cms.InputTag("ThreeProngInputSelectorStep2", "InputTauRefs")
+process.KinematicTauProducer.inputTracks = cms.InputTag("ThreeProngInputSelectorStep2", "InputTracks")
 
 ### event storage
 process.load("ACFrameworkModules.FinalTreeFiller.FinalTreeFiller_cfi")
@@ -105,7 +107,7 @@ process.ignorePath = cms.Path(
     *cms.ignore(process.PrimVtxSelector)
     #*cms.ignore(process.HLTSelector)
     *cms.ignore(process.InputTrackSelector)
-    *cms.ignore(process.ThreeProngInputSelector)
+    *process.ignoreThreeProngInputSelector
     *cms.ignore(process.KinematicTauProducer)
     *process.matchingSeq
     *process.FinalTreeFiller
@@ -127,4 +129,3 @@ if ignoreFilter:
 	process.schedule = cms.Schedule(process.ignorePath)
 else:
 	process.schedule = cms.Schedule(process.cumulativePath)
-
