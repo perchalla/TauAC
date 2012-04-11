067feec10a58d31252692f3c20bb2146a814fd80
perchall <>
2012-04-11 10:26:08 +0000

    prepare for next round of skims

--- ACFrameworkModules/Common/python/Tools.py
+++ ACFrameworkModules/Common/python/Tools.py
@@ -21,25 +21,26 @@ def buildPoolSource(files):
 def parseDecayType(jobName):
     decayType = 'unknown'
     motherPdgID = 0
-    if jobName.find('tau3pi')!=-1:
-        if jobName.find('tau3piFromVBFH')!=-1:
+    if 'tau3pi' in jobName:
+        if 'tau3piFromVBFH' in jobName:
             decayType = 'VBFH3pr'
-        if jobName.find('tau3piFromGFH')!=-1:
+        if 'tau3piFromGFH' in jobName:
             decayType = 'GFH3pr'
-        if jobName.find('tau3piFromZ')!=-1:
+        if 'tau3piFromZ' in jobName:
             decayType = 'Z3pr'
-        if jobName.find('tau3piFromW')!=-1:
+        if 'tau3piFromW' in jobName:
             decayType = 'W3pr'
     else:  
-        if jobName.find('FromZ')!=-1:
+        if 'FromZ' in jobName:
             decayType = 'TauPair'
             motherPdgID = 23
-        if jobName.find('FromW')!=-1:
+        if 'FromGFH' in jobName or 'FromVBFH' in jobName or 'GFHTauTau' in jobName:
+            decayType = 'TauPair'
+            motherPdgID = 25
+        if 'FromW' in jobName:
             print 'Decay mode not yet covered!'
         #decayType = 'TauPair'
         #motherPdgID = 23
-        if jobName.find('FromGFH')!=-1 or jobName.find('FromVBFH')!=-1:
-            decayType = 'TauPair'
-            motherPdgID = 25
 
+    #print 'jobname, decayType, motherPdgID: ',jobName, decayType, motherPdgID
     return [decayType, motherPdgID]
--- ACFrameworkModules/FinalTreeFiller/python/Steering.py
+++ ACFrameworkModules/FinalTreeFiller/python/Steering.py
@@ -5,12 +5,12 @@ baseDir = os.path.relpath(os.environ.get('CMSSW_BASE')+"/src")
 
 def LoadUserParameters():
     username = 'perchalla'
-    outputPath = '/user/'+username+'/output/analysis/CMSSW_4_4_2/PVBugFix/'
+    outputPath = '/user/'+username+'/output/analysis/CMSSW_4_4_2/TauAC_V01-08-01/'
 
 #    jsonFile = baseDir+"/ACFrameworkModules/FinalTreeFiller/Cert_160404-180252_7TeV_PromptReco_Collisions11_JSON.txt"
     jsonFile = baseDir+"/ACFrameworkModules/FinalTreeFiller/Cert_160404-180252_7TeV_ReRecoNov08_Collisions11_JSON.txt"
 
-    jobName = 'TauNov2011'
+    jobName = 'tauSMBrFromZ'
 
     inputPath = ''
     globaltag = ''
@@ -31,18 +31,43 @@ def LoadUserParameters():
         ##/QCD_Pt-30to80_EMEnriched_TuneZ2_7TeV-pythia/Fall11-PU_S6-START44_V5-v1/AODSIM
         inputPath = 'Fall11/QCD_Pt-30to80_EMEnriched_TuneZ2_7TeV-pythia/AODSIM/PU_S6-START44_V5-v1/'
         globaltag = 'START44_V12::All'
-    elif jobName == 'TauNov2011':
+    elif jobName == 'WJetsToLNu':
+        ##/WJetsToLNu_TuneZ2_7TeV-madgraph-tauola/Fall11-PU_S6-START44_V5-v1/AODSIM
+        inputPath = 'Fall11/QCD_Pt-30to80_EMEnriched_TuneZ2_7TeV-pythia/AODSIM/PU_S6-START44_V5-v1/'
+        globaltag = 'START44_V12::All'
+    elif jobName == 'TauNov2011A':
         ##/Tau/Run2011A-08Nov2011-v1/AOD
         inputPath = 'Run2011A/Tau/AOD/08Nov2011-v1/'
-        globaltag = 'GR_R_44_V14::All'
-    elif jobName == 'TauPlusXNov2011':
+        globaltag = 'GR_R_44_V15::All'
+    elif jobName == 'TauNov2011B':
+        ##/Tau/Run2011B-19Nov2011-v1/AOD
+        inputPath = ''
+        globaltag = 'GR_R_44_V15::All'
+    elif jobName == 'TauPlusXNov2011A':
         ##/TauPlusX/Run2011A-08Nov2011-v1/AOD
         inputPath = 'Run2011A/TauPlusX/AOD/08Nov2011-v1/'
-        globaltag = 'GR_R_44_V14::All'
+        globaltag = 'GR_R_44_V15::All'
+    elif jobName == 'TauPlusXNov2011B':
+        ##/TauPlusX/Run2011A-19Nov2011-v1/AOD
+        inputPath = ''
+        globaltag = 'GR_R_44_V15::All'
     elif jobName == 'MinimumBias':
         ##/MinimumBias/Run2011A-PromptReco-v6/AOD   globaltag='GR_P_V22::All'
         inputPath = 'Run2011A/MinimumBias/AOD/PromptReco-v6/'
-        globaltag = 'GR_P_V27::All'
+        globaltag = 'GR_R_44_V15::All'
+    elif jobName == 'GFHTauTauM115':
+        ##/GluGluToHToTauTau_M-115_7TeV-powheg-pythia6/Fall11-PU_S6_START44_V9B-v1/AODSIM
+        inputPath = ''
+        globaltag = 'START44_V12::All'
+    elif jobName == 'GFHTauTauM125':
+        ##/GluGluToHToTauTau_M-125_7TeV-powheg-pythia6/Fall11-PU_S6_START44_V9B-v1/AODSIM
+        inputPath = ''
+        globaltag = 'START44_V12::All'
+    elif jobName == 'GFHTauTauM135':
+        ##/GluGluToHToTauTau_M-135_7TeV-powheg-pythia6/Fall11-PU_S6_START44_V9B-v1/AODSIM
+        inputPath = 'Fall11/GluGluToHToTauTau_M-135_7TeV-powheg-pythia6/AODSIM/PU_S6_START42_V14B-v1/'
+        globaltag = 'START44_V12::All'
+
     
     isData = False #data or MC
     if inputPath.find('Run20', 0, 5) != -1:
